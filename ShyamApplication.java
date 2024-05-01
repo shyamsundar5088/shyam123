d00611bc5c548100069141c2beb40c9521f33bf0
devanshu <devanshugupta768@gmail.com>
2024-05-01 18:24:12 +0530

    Initial commit

---
+++ ShyamApplication.java
@@ -0,0 +1,13 @@
+package staragile.assignment.shyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShyamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShyamApplication.class, args);
+	}
+
+}
